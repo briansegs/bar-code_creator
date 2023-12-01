2227deba1cc5c72d2ad8cc78484006f41b0145e6
bsegers <briansegs@gmail.com>
2023-12-01 14:20:14 -0500

    docs: Add TODOs

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from random import randint
 from barcode import EAN13
 from barcode.writer import ImageWriter
 # TODO: turn into class and then import
+# TODO: Add print statments to edit prefix or use default
+# TODO: Add welcome print statment 
+# TODO: Add option to give a barcode number or generate a random one
 with open("data.json", "r", encoding="utf-8") as file:
     data = json.load(file)
     file.close()
