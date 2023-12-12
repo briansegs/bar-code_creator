c000e092d890900533085174fcb399071d720c7e
bsegers <briansegs@gmail.com>
2023-12-11 21:21:42 -0500

    docs: Remove TODOs

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from time import sleep
 from barcodeCreator import BarcodeCreator
 from data import data, errorMsg
 
-# TODO: Make a version of the app and class that will work without the json
-# and saves the barcode to the root dir (default for EAN13().save())
-# This version will be for the public. More user friendly
-
 print("Let's create an EAN13 barcode")
 sleep(2)
 
