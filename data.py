282e7a732033664c8c000206aa5a24314c1b3265
bsegers <briansegs@gmail.com>
2023-12-14 21:49:56 -0500

    add: Print options

--- data.py
+++ data.py
@@ -3,12 +3,17 @@ from time import sleep
 
 data = {
     "output_folder" : "barcodes",
+    "prefix" : "101",
     "options" : {
         "1" : "Use your own 13 digit number",
         "2" : "Generate a random 13 digit number",
         "3" : "Input a 3 digit prefix followed by a random 10 digit number"
     },
-    "prefix" : "101"
+    "print_options" : {
+        "1" : "Use barcode number",
+        "2" : "Use your own custom name",
+        "3" : "Use a custom name combined with the barcode number"
+    }
 }
 
 def errorMsg(error):
